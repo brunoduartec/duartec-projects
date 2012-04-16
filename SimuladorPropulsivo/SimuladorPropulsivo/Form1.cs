609a3bc90a3b08585ae3dc673da3b688362a7c58
bruno.duartec <bruno.duartec@ea514589-e3f4-cad8-24f1-9a6f8281c3a9>
2012-04-16 22:01:40 +0000

    Adding project

---
+++ SimuladorPropulsivo/SimuladorPropulsivo/Form1.cs
@@ -0,0 +1,524 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimuladorPropulsivo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+
+        public PropulsiveSolver solver = new PropulsiveSolver();
+
+        public void Init()
+        {
+            solver.InitGains(0.97, 0.85, 1.00, 0.90, 0.98, 1.00, 0.85, 0.97);
+            solver.InitGamas(1.36, 1.40, 1.37, 1.33, 1.36);
+
+
+
+        }
+
+
+
+        public void DrawGraph()
+        {
+
+
+            
+
+            Graphics g = pictureBox1.CreateGraphics();
+  g.Clear(Color.White);
+
+
+            Pen PressurePen = new Pen(Color.Red);
+            PressurePen.Width = 2;
+
+          
+
+            float total = pictureBox1.Width;
+            float cima = pictureBox1.Height;
+
+            float yfact = (float)( pictureBox1.Height / (solver.MaxP));
+
+            float fixval = 10000;
+            g.DrawLines(PressurePen, new PointF[] 
+            {
+
+                  
+                new PointF(0,cima - float.Parse(solver.Variables["PA"].ToString())/fixval), 
+                new PointF (4*total/15.0f,cima - float.Parse(solver.Variables["P02"].ToString())*yfact), 
+                new PointF (7*total/15.0f,cima - float.Parse(solver.Variables["P03"].ToString())*yfact), 
+                new PointF (9*total/15.0f,cima - float.Parse(solver.Variables["P04"].ToString())*yfact), 
+                new PointF (10*total/15.0f,cima - float.Parse(solver.Variables["P05"].ToString())*yfact), 
+                new PointF (15*total/15.0f,cima - float.Parse(solver.Variables["P06"].ToString())*yfact)
+              
+               
+            });
+
+
+            yfact = (float)(pictureBox1.Height / (solver.MaxT));
+
+            Pen TemperaturePen = new Pen(Color.Blue);
+            TemperaturePen.Width = 2;
+
+
+            fixval = 10;
+          
+            g.DrawLines(TemperaturePen, new PointF[] 
+            {
+                new PointF(0,cima - float.Parse(solver.Variables["TA"].ToString())/fixval), 
+                new PointF (4*total/15.0f,cima - float.Parse(solver.Variables["T02"].ToString())*yfact), 
+                new PointF (7*total/15.0f,cima - float.Parse(solver.Variables["T03"].ToString())*yfact), 
+                new PointF (9*total/15.0f,cima - float.Parse(solver.Variables["T04"].ToString())*yfact), 
+                new PointF (10*total/15.0f,cima - float.Parse(solver.Variables["T05"].ToString())*yfact), 
+                new PointF (15*total/15.0f,cima - float.Parse(solver.Variables["T06"].ToString())*yfact)
+              
+               
+            });
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            solver.SolveSystem(radioButton2.Checked,false,double.Parse(textM.Text), double.Parse(textPa.Text), double.Parse(textTa.Text), double.Parse(textPC.Text), double.Parse(textR.Text), double.Parse(textCP.Text), double.Parse(textTqueima.Text), double.Parse(textTposqueima.Text), double.Parse(textPrf.Text), double.Parse(textPrc.Text), double.Parse(textB.Text));
+
+            //solver.SolveSystem(0.85,18750,216.70,45000,288.3,1.08,1600,0,1.5,20,0,0);
+
+            LabelEmpuxo.Text = "Empuxo = " + solver.Empuxo.ToString();
+            LabelUs.Text = "TSFC = " + solver.TSFC.ToString();
+
+            DrawGraph();
+
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textM.Enabled = true;
+            textPa.Enabled = true;
+            textTa.Enabled = true;
+            textPC.Enabled = true;
+            textR.Enabled = true;
+            textCP.Enabled = true;
+            textTqueima.Enabled = true;
+            textTposqueima.Enabled = true;
+            textPrf.Enabled = true;
+            textPrc.Enabled = true;
+            textB.Enabled = true;
+            solver.actualtype = PropulsiveSolver.TYPE.TURBOFAN;
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+
+            textM.Enabled = true;
+            textPa.Enabled = true;
+            textTa.Enabled = true;
+            textPC.Enabled = true;
+            textR.Enabled = true;
+            textCP.Enabled = true;
+            textTqueima.Enabled = true;
+            textTposqueima.Enabled = true;
+
+            textPrf.Enabled = false;
+            textPrf.Text = "1";
+
+            textPrc.Enabled = true;
+
+            textB.Enabled = false;
+            textB.Text = "0";
+            solver.actualtype = PropulsiveSolver.TYPE.TURBOJATO;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textM.Enabled = true;
+            textPa.Enabled = true;
+            textTa.Enabled = true;
+            textPC.Enabled = true;
+            textR.Enabled = true;
+            textCP.Enabled = true;
+            textTqueima.Enabled = true;
+            textTposqueima.Enabled = true;
+
+
+            textPrf.Enabled = false;
+            textPrf.Text = "1";
+
+            textPrc.Enabled = false;
+            textPrc.Text = "1";
+
+            textB.Enabled = false;
+            textB.Text = "0";
+
+            solver.actualtype = PropulsiveSolver.TYPE.RAMJET;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double Btemp = double.Parse(textB.Text);
+
+
+            List<PointF> listb = new List<PointF>();
+
+
+            List<double> tempvals = new List<double>();
+
+
+
+            int pontos = int.Parse(textPontos.Text);
+            int xmax = int.Parse( textxmax.Text);
+            float delta = (float)xmax / pontos;
+            float xval = 0;
+
+            int i = 0;
+
+            float xGraphMax = pontos;
+            //for (int i = 0; i < 9*passo; i++)
+
+            double ytemp;
+
+            CsvExport myExport = new CsvExport();
+            bool flag = false;
+            while (i < pontos && !flag)
+            {
+                i++;
+                xval += delta;
+
+
+                myExport.AddRow();
+                myExport["B"] = xval;
+
+
+                #region T1300
+                solver.SolveSystem(radioButton2.Checked, false, double.Parse(textM.Text), double.Parse(textPa.Text), double.Parse(textTa.Text), double.Parse(textPC.Text), double.Parse(textR.Text), double.Parse(textCP.Text), 1300, double.Parse(textTposqueima.Text), double.Parse(textPrf.Text), double.Parse(textPrc.Text), xval);
+
+                ytemp = solver.Empuxo;
+
+
+
+                //if (ytemp.Equals(double.NaN)) // nesse caso acabou o ponto em que o grafico tem pontos validos
+                //{
+                //    xGraphMax = i - 1;
+
+                //    break;
+                //}
+
+                //else
+                //{
+                //    tempvals.Add(ytemp);
+                //}
+
+
+                if (!ytemp.Equals(double.NaN)) // nesse caso acabou o ponto em que o grafico tem pontos validos
+                {
+                 
+                    myExport["Empuxo1.3k"] = ytemp;
+                }
+                else
+                {
+                    myExport["Empuxo1.3k"] = "";
+                }
+
+                
+                
+                #endregion
+
+                #region T1500
+                solver.SolveSystem(radioButton2.Checked, false, double.Parse(textM.Text), double.Parse(textPa.Text), double.Parse(textTa.Text), double.Parse(textPC.Text), double.Parse(textR.Text), double.Parse(textCP.Text), 1500, double.Parse(textTposqueima.Text), double.Parse(textPrf.Text), double.Parse(textPrc.Text), xval);
+
+                ytemp = solver.Empuxo;
+
+                if (!ytemp.Equals(double.NaN)) // nesse caso acabou o ponto em que o grafico tem pontos validos
+                myExport["Empuxo1.5k"] = ytemp;
+                else
+                {
+                    myExport["Empuxo1.5k"] = "";
+                }
+
+                #endregion
+
+
+                #region T1700
+                solver.SolveSystem(radioButton2.Checked, false, double.Parse(textM.Text), double.Parse(textPa.Text), double.Parse(textTa.Text), double.Parse(textPC.Text), double.Parse(textR.Text), double.Parse(textCP.Text),1700, double.Parse(textTposqueima.Text), double.Parse(textPrf.Text), double.Parse(textPrc.Text), xval);
+
+                ytemp = solver.Empuxo;
+
+                if (!ytemp.Equals(double.NaN)) // nesse caso acabou o ponto em que o grafico tem pontos validos
+                myExport["Empuxo1.7k"] = ytemp;
+                else
+                {
+                    myExport["Empuxo1.7k"] = "";
+                }
+
+                #endregion
+
+
+                
+
+                //if (ytemp.Equals(double.NaN)) // nesse caso acabou o ponto em que o grafico tem pontos validos
+                //{
+                //    xGraphMax = i - 1;
+
+                //    break;
+                //}
+
+                //else
+                //{
+                //    tempvals.Add(ytemp);
+                //}
+
+
+              
+
+
+            }
+
+
+            //List<double> ttval = tempvals;
+
+
+            //ttval.Sort();
+
+            //double ymin = ttval[0];
+            //double ymax = ttval[ttval.Count - 1];
+
+
+            //xval = 0;
+
+
+         
+            //for (int ii = 0; ii < xGraphMax; ii++)
+            //{
+
+            //    //float xp = (float)((ImageBox1.Width / xmax) * xval);
+            //    xval += delta;
+
+            //    //float yp = (float)((tempvals[ii] - ymin) * ImageBox1.Height / (ymax - ymin));
+
+            //    //PointF pp = new PointF(xp, ImageBox1.Height - yp);
+            //    //listb.Add(pp);
+
+            //    myExport.AddRow();
+            //    myExport["B"] = xval;
+            //    myExport["Empuxo"] = tempvals[ii];
+
+            //}
+
+            myExport.ExportToFile(AppDomain.CurrentDomain.BaseDirectory + "EMPUXO-"+solver.actualtype.ToString()+".csv");
+
+            //Graphics g = ImageBox1.CreateGraphics();
+
+            //Pen myPen = new Pen(Color.Red);
+            //myPen.Width = 2;
+
+            //g.Clear(Color.White);
+
+            //g.DrawLines(myPen, listb.ToArray());
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double Btemp = double.Parse(textB.Text);
+
+
+            List<PointF> listb = new List<PointF>();
+
+
+            List<double> tempvals = new List<double>();
+
+
+
+            int pontos = int.Parse(textPontos.Text);
+            int xmax = int.Parse(textxmax.Text);
+            float delta = (float)xmax / pontos;
+            float xval = 0;
+
+            int i = 0;
+
+            float xGraphMax = pontos;
+            //for (int i = 0; i < 9*passo; i++)
+
+            double ytemp;
+
+            CsvExport myExport = new CsvExport();
+            bool flag = false;
+            while (i < pontos && !flag)
+            {
+                i++;
+                xval += delta;
+
+
+                myExport.AddRow();
+                myExport["B"] = xval;
+
+
+                #region T1300
+                solver.SolveSystem(radioButton2.Checked, false, double.Parse(textM.Text), double.Parse(textPa.Text), double.Parse(textTa.Text), double.Parse(textPC.Text), double.Parse(textR.Text), double.Parse(textCP.Text), 1300, double.Parse(textTposqueima.Text), double.Parse(textPrf.Text), double.Parse(textPrc.Text), xval);
+
+                ytemp = solver.TSFC;
+
+
+
+                //if (ytemp.Equals(double.NaN)) // nesse caso acabou o ponto em que o grafico tem pontos validos
+                //{
+                //    xGraphMax = i - 1;
+
+                //    break;
+                //}
+
+                //else
+                //{
+                //    tempvals.Add(ytemp);
+                //}
+
+
+                if (!ytemp.Equals(double.NaN)) // nesse caso acabou o ponto em que o grafico tem pontos validos
+                {
+
+                    myExport["TSFC-1.3k"] = ytemp;
+                }
+                else
+                {
+                    myExport["TSFC-1.3k"] = "";
+                }
+
+
+
+                #endregion
+
+                #region T1500
+                solver.SolveSystem(radioButton2.Checked, false, double.Parse(textM.Text), double.Parse(textPa.Text), double.Parse(textTa.Text), double.Parse(textPC.Text), double.Parse(textR.Text), double.Parse(textCP.Text), 1500, double.Parse(textTposqueima.Text), double.Parse(textPrf.Text), double.Parse(textPrc.Text), xval);
+
+                ytemp = solver.TSFC;
+
+                if (!ytemp.Equals(double.NaN)) // nesse caso acabou o ponto em que o grafico tem pontos validos
+                    myExport["TSFC-1.5k"] = ytemp;
+                else
+                {
+                    myExport["TSFC-1.5k"] = "";
+                }
+
+                #endregion
+
+
+                #region T1700
+                solver.SolveSystem(radioButton2.Checked, false, double.Parse(textM.Text), double.Parse(textPa.Text), double.Parse(textTa.Text), double.Parse(textPC.Text), double.Parse(textR.Text), double.Parse(textCP.Text), 1700, double.Parse(textTposqueima.Text), double.Parse(textPrf.Text), double.Parse(textPrc.Text), xval);
+
+                ytemp = solver.TSFC;
+
+                if (!ytemp.Equals(double.NaN)) // nesse caso acabou o ponto em que o grafico tem pontos validos
+                    myExport["TSFC-1.7k"] = ytemp;
+                else
+                {
+                    myExport["TSFC-1.7k"] = "";
+                }
+
+                #endregion
+
+
+
+
+                //if (ytemp.Equals(double.NaN)) // nesse caso acabou o ponto em que o grafico tem pontos validos
+                //{
+                //    xGraphMax = i - 1;
+
+                //    break;
+                //}
+
+                //else
+                //{
+                //    tempvals.Add(ytemp);
+                //}
+
+
+
+
+
+            }
+
+
+            //List<double> ttval = tempvals;
+
+
+            //ttval.Sort();
+
+            //double ymin = ttval[0];
+            //double ymax = ttval[ttval.Count - 1];
+
+
+            //xval = 0;
+
+
+
+            //for (int ii = 0; ii < xGraphMax; ii++)
+            //{
+
+            //    //float xp = (float)((ImageBox1.Width / xmax) * xval);
+            //    xval += delta;
+
+            //    //float yp = (float)((tempvals[ii] - ymin) * ImageBox1.Height / (ymax - ymin));
+
+            //    //PointF pp = new PointF(xp, ImageBox1.Height - yp);
+            //    //listb.Add(pp);
+
+            //    myExport.AddRow();
+            //    myExport["B"] = xval;
+            //    myExport["Empuxo"] = tempvals[ii];
+
+            //}
+
+            myExport.ExportToFile(AppDomain.CurrentDomain.BaseDirectory + "TSFC-" + solver.actualtype.ToString() + ".csv");
+
+            //Graphics g = ImageBox1.CreateGraphics();
+
+            //Pen myPen = new Pen(Color.Red);
+            //myPen.Width = 2;
+
+            //g.Clear(Color.White);
+
+            //g.DrawLines(myPen, listb.ToArray());
+
+        }
+
+
+
+
+
+    }
+}
---
+++ SimuladorPropulsivo/SimuladorPropulsivo/PropulsiveSolver.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimuladorPropulsivo
+{
+    public class PropulsiveSolver
+    {
+
+        private Dictionary<string, double> variables = new Dictionary<string, double>();
+
+
+        double empuxo;
+
+        public double Empuxo
+        {
+            get { return empuxo; }
+            set { empuxo = value; }
+        }
+        double tSFC;
+
+        public double TSFC
+        {
+            get { return tSFC; }
+            set { tSFC = value; }
+        }
+
+        public enum TYPE
+        {
+            TURBOJATO,
+            TURBOFAN,
+            RAMJET
+
+        }
+
+        public TYPE actualtype;
+
+        #region Initial Entries
+        double gamad;
+        double gamaf;
+        double gamac;
+        double gamat;
+        double gaman;
+
+
+        double Nd;
+        double Nc;
+        double Nb;
+        double Nt;
+        double Nn;
+        double Nab;
+        double Nf;
+        double Nfh;
+
+
+        #endregion
+
+        double maxP;
+
+        public double MaxP
+        {
+            get { return maxP; }
+            set { maxP = value; }
+        }
+        double maxT;
+
+        public double MaxT
+        {
+            get { return maxT; }
+            set { maxT = value; }
+        }
+
+
+        public Dictionary<string, double> Variables
+        {
+            get { return variables; }
+            set { variables = value; }
+        }
+        public void InitGamas(double gamad, double gamaf, double gamac, double gamat, double gaman)
+        {
+            this.gamad = gamad;
+            this.gamaf = gamaf;
+            this.gamac = gamac;
+            this.gamat = gamat;
+            this.gaman = gaman;
+        }
+
+        public void InitGains(double Nd, double Nc, double Nb, double Nt, double Nn, double Nab, double Nf, double Nfh)
+        {
+
+
+            this.Nd = Nd;
+            this.Nc = Nc;
+            this.Nb = Nb;
+            this.Nt = Nt;
+            this.Nn = Nn;
+            this.Nab = Nab;
+            this.Nf = Nf;
+            this.Nfh = Nfh;
+
+
+        }
+        bool hasfan;
+        bool afterburn;
+
+
+        public void SolveSystem(bool hasfan, bool afterburn, double M, double Pa, double Ta, double PC, double R, double Cp, double T04, double T06, double Prf, double Prc, double B)
+        {
+            
+            this.hasfan = hasfan;
+            this.afterburn = afterburn;
+            variables.Clear();
+            variables.Add("TA", Ta);
+            variables.Add("PA", Pa);
+
+            double T02 = Ta * (1 + (gamad - 1) / 2 * Math.Pow(M, 2));
+
+
+            double P02 = Pa * Math.Pow((1 + Nd * (T02 / Ta - 1)), (gamad / (gamad - 1)));
+
+
+            variables.Add("T02", T02);
+            variables.Add("P02", P02);
+
+            double T08 = T02 * (1 + (1 / Nf) * (Math.Pow(Prf, ((gamaf - 1) / gamaf)) - 1));
+            double P08 = P02 * Prf;
+
+            variables.Add("T08", T08);
+            variables.Add("P08", P08);
+
+            double Usf = Math.Sqrt(2 * Nfh * (gamaf / (gamaf - 1)) * R * T08 * (1 - Math.Pow((Pa / P08), ((gamaf - 1) / gamaf))));
+
+            double P03;
+            double T03;
+
+            if (hasfan)
+            {
+                B = 0;
+                P03 = P08 * Prc;
+                T03 = T08 * (1 + (1 / Nc) * (Math.Pow(Prc, ((gamac - 1) / gamac)) - 1));
+            }
+            else
+            {
+                P03 = P02 * Prc;
+                T03 = T02 * (1 + (1 / Nc) * (Math.Pow(Prc, ((gamac - 1) / gamac)) - 1));
+            }
+
+            variables.Add("T03", T03);
+            variables.Add("P03", P03);
+
+
+            maxP = P03;
+
+
+            double f1 = (T04 / T03 - 1) / ((Nb * PC) / (Cp * T03) - T04 / T03);
+            double P04 = P03;
+
+            variables.Add("T04", T04);
+            variables.Add("P04", P04);
+
+            double T05 = T04 - (T03 - T02) - B * (T08 - T02);
+            double P05 = P04 * Math.Pow((1 - (1 / Nt) * (1 - T05 / T04)), (gamat / (gamat - 1)));
+
+
+            variables.Add("T05", T05);
+            variables.Add("P05", P05);
+
+            double f2 = (T06 / T05 - 1) / ((Nab * PC) / (Cp * T05) - T06 / T05);
+            double P06 = P05;
+
+            variables.Add("T06", T06);
+            variables.Add("P06", P06);
+
+
+            maxT = T06;
+            double f;
+
+
+            if (afterburn)
+            {
+                f = f1 + f2;
+            }
+            else
+            {
+                f = f1;
+            }
+            double us = Math.Sqrt(2 * Nn * (gaman / (gaman - 1)) * R * T05 * (1 - Math.Pow((Pa / P05), ((gaman - 1) / gaman))));
+
+            double u = M * Math.Sqrt(gamad * R * Ta);
+
+            Empuxo = ((1 + f) * us + B * Usf - (B + 1) * u); // considerando kilonewtons
+            TSFC = f / Empuxo * 1000;
+        }
+
+
+
+    }
+}
