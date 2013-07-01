b36527eb9872fdd78082428735e8b08da7292400
bruno.duartec <bruno.duartec@ea514589-e3f4-cad8-24f1-9a6f8281c3a9>
2013-07-01 01:51:14 +0000

    Trying to add MySQLConnection

---
+++ trunk/GameRanking/Development/RankingService/RankingService/DAO/MySQLConnection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace RankingService.DAO
+{
+    public class MySQLConnection
+    {
+
+        private static MySQLConnection instance;
+        private bool Connstatus = false;
+        public static MySQLConnection Instance()
+        {
+            if (instance == null)
+            {
+                instance = new MySQLConnection();
+            }
+
+            return instance;
+        
+        
+        }
+
+        private MySqlConnection connection;
+
+        public bool GetConnectionStatus()
+        {
+
+            return Connstatus;
+        
+        }
+
+        private void CreateConnection()
+        {
+            
+            try
+            {
+                string host = ConfigurationManager.ConnectionStrings["MySQLConnString"].ConnectionString;
+
+
+                string MyConString = "server=dbmy0052.whservidor.com;User Id=ploobs_8;Persist Security Info=True;database=ploobs_8;pooling=false;";
+                connection = new MySqlConnection(MyConString);
+                Connstatus = true;
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+
+            
+        }
+        
+        protected MySQLConnection()
+        {
+            CreateConnection();
+        
+        
+        
+        }
+    }
+}
