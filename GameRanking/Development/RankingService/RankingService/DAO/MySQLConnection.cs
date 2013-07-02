29ed0e915978e7a843edb5dc582cb14d35196eda
bruno.duartec <bruno.duartec@ea514589-e3f4-cad8-24f1-9a6f8281c3a9>
2013-07-02 03:34:02 +0000

    Adding the Web Consumer

--- GameRanking/Development/RankingService/RankingService/DAO/MySQLConnection.cs
+++ GameRanking/Development/RankingService/RankingService/DAO/MySQLConnection.cs
@@ -20,8 +20,8 @@ namespace RankingService.DAO
             }
 
             return instance;
-        
-        
+
+
         }
 
         private MySqlConnection connection;
@@ -30,36 +30,103 @@ namespace RankingService.DAO
         {
 
             return Connstatus;
-        
+
         }
 
         private void CreateConnection()
         {
-            
+
             try
             {
-                string host = ConfigurationManager.ConnectionStrings["MySQLConnString"].ConnectionString;
+                //string host = ConfigurationManager.ConnectionStrings["MySQLConnString"].ConnectionString;
 
 
-                string MyConString = "server=dbmy0052.whservidor.com;User Id=ploobs_8;Persist Security Info=True;database=ploobs_8;pooling=false;";
+                string MyConString = "server=dbmy0052.whservidor.com;UID=ploobs_8; PASSWORD=gameranking1;database=ploobs_8";
                 connection = new MySqlConnection(MyConString);
                 Connstatus = true;
             }
             catch (Exception)
             {
-                
+
                 throw;
             }
 
-            
+
         }
-        
+
+        public void SQLQuery(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = query;
+            cmd.Connection = connection;
+            connection.Open();
+            int aff = cmd.ExecuteNonQuery();
+
+
+            connection.Close();
+        }
+
+        public void SQLInsertImage(String Name, byte[] buffer)
+        {
+            String name_new = Name;
+            byte[] buffer_new = buffer;
+
+            connection.Open();
+            MySqlCommand command = new MySqlCommand("", connection);
+            command.CommandText = "insert into User(Name,Image) values(@name,@image);";
+
+            command.Parameters.AddWithValue("@name", name_new);
+            command.Parameters.AddWithValue("@image", buffer_new);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
+
+        }
+
+
+        public List<Object> SQLQueryReturn(string query)
+        {
+
+
+            List<Object> fragments = new List<Object>();
+
+
+            MySqlCommand command = connection.CreateCommand();
+            MySqlDataReader Reader;
+            command.CommandText = query;
+            connection.Open();
+            Reader = command.ExecuteReader();
+
+
+            int items = Reader.FieldCount;// retrieving the size of the object
+            while (Reader.Read())
+            {
+                string stringreturn = "";
+                List<Object> subitem = new List<object>();
+
+                for (int i = 1; i < items; i++)
+                {
+                    subitem.Add(Reader.GetValue(i));
+                    //stringreturn += (Reader.GetValue(i) + ",");
+                }
+
+               // stringreturn = stringreturn.Remove(stringreturn.Length - 1);
+                fragments.Add(subitem);
+
+            }
+            connection.Close();
+            return fragments;
+
+        }
+
         protected MySQLConnection()
         {
             CreateConnection();
-        
-        
-        
+
+
+
         }
     }
 }
