e953654f9ff04f610479c1fa581fcf10d5a79dc7
bruno.duartec <bruno.duartec@ea514589-e3f4-cad8-24f1-9a6f8281c3a9>
2014-03-23 23:40:12 +0000

    Adding QA Tools

---
+++ QATools/QATools/ExcelTools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace QATools
+{
+    class ExcelTools
+    {
+
+
+        public ExcelTools()
+        {
+
+            
+
+
+        }
+
+        
+    }
+}
---
+++ QATools/QATools/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace QATools
+{
+   public class Item
+    {
+         [XmlElement("label")]
+       public string label;
+         [XmlElement("value")]
+       public string value;
+    }
+}
