79798605422fa574f58cf4d4878c3ad8bc63171b
bruno.duartec <bruno.duartec@ea514589-e3f4-cad8-24f1-9a6f8281c3a9>
2014-06-19 15:59:59 +0000

    corrigindo acentuação

--- trunk/Semear/semeares/2014/sobre/default.php
+++ trunk/Semear/semeares/2014/sobre/default.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
 
 <!-- #BeginTemplate "../master.dwt" -->
@@ -56,11 +56,11 @@
 	<div id="navigation">
 		<ul>
 			<li><a href="../default.htm">Home</a></li>
-			<li><a href="../programacao/default.php">Programa��o</a></li>
+			<li><a href="../programacao/default.php">Programação</a></li>
 			<li><a href="default.php">Sobre o Instituto</a></li>
 			<li><a href="../imprensa/default.php">Na imprensa</a></li>
 			<li><a href="../galeria/default.php">Galeria</a></li>
-			<li><a href="../inscricao/default.php">Inscri��es</a></li>
+			<li><a href="../inscricao/default.php">Inscrições</a></li>
 			<li><a href="../local/default.php">Local</a></li>
 			<li><a href="../contato/default.php">Contato</a></li>
 		</ul>
@@ -76,84 +76,83 @@
 	
 			
 			
-			<h4>Hist&#65533;rico institucional</h4> 
+			<h4>Histórico institucional</h4> 
 
 <p> 
-No in&#65533;cio deste s&#65533;culo, observou-se em S&#65533;o Jos&#65533; dos Campos-SP que cerca de 40% dos 
+No início deste século, observou-se em São José dos Campos-SP que cerca de 40% dos 
 estudantes das classes C e D, aprovados em universidades e com grande potencial de 
-desenvolvimento, abandonam seus cursos por quest&#65533;es socioecon&#65533;micas. 
+desenvolvimento, abandonam seus cursos por questões socioeconômicas. 
 </p>
 
 <p>
 Para minimizar esse problema social, foi fundado o Instituto Semear. Trata-se de uma Entidade de 
-Interesse Social cujo objetivo &#65533; dar apoio a jovens talentos de classes sociais menos favorecidas de 
-forma que eles obtenham &#65533;xito em suas gradua&#65533;&#65533;es. 
+Interesse Social cujo objetivo é dar apoio a jovens talentos de classes sociais menos favorecidas de 
+forma que eles obtenham êxito em suas graduações. 
  </p>
  
-<h4>Miss&#65533;o </h4>
+<h4>Missão </h4>
 <p>
-Desenvolver e conectar, por meio do incentivo &#65533; educa&#65533;&#65533;o, l&#65533;deres comprometidos com os desafios 
+Desenvolver e conectar, por meio do incentivo à educação, líderes comprometidos com os desafios 
 sociais e que atuem como agentes multiplicadores para um Brasil melhor e mais justo. 
  </p>
  
-<h4>Vis&#65533;o </h4>
+<h4>Visão </h4>
 <p>
-Tornar-se modelo de institui&#65533;&#65533;o que apoia e insere universit&#65533;rios financeiramente desfavorecidos 
-para atuarem em prol de um pa&#65533;s melhor e mais justo. 
+Tornar-se modelo de instituição que apoia e insere universitários financeiramente desfavorecidos 
+para atuarem em prol de um país melhor e mais justo. 
  </p>
  
 <h4>Valores </h4>
 
 <p>
-Energia Jovem: pessoas inspiradas por prop&#65533;sitos nobres possuem uma capacidade incompar&#65533;vel 
-de transforma&#65533;&#65533;o. 
+Energia Jovem: pessoas inspiradas por propósitos nobres possuem uma capacidade incomparável 
+de transformação. 
 </p>
 <p>
-Disciplina Consciente: um pa&#65533;s melhor e mais justo deve ser embebido de &#65533;tica e integridade. A 
-consci&#65533;ncia da honestidade de todos constr&#65533;i rela&#65533;&#65533;es de confian&#65533;a s&#65533;lidas e produtivas.  
+Disciplina Consciente: um país melhor e mais justo deve ser embebido de ética e integridade. A 
+consciência da honestidade de todos constrói relações de confiança sólidas e produtivas.  
 </p>
 <p>
 Atitude Empreendedora: o tamanho da conquista depende do tamanho dos sonhos. Sonhar 
-grande, come&#65533;ar pequeno e andar r&#65533;pido para criar mudan&#65533;as sociais positivas. 
+grande, começar pequeno e andar rápido para criar mudanças sociais positivas. 
 </p>
 <p>
 Comunidade: inspirar e apoiar pessoas para se tornarem agentes transformadores da comunidade 
 onde vivem. 
-Educa&#65533;&#65533;o como instrumento de desenvolvimento econ&#65533;mico e social: cren&#65533;a no poder 
-transformador da educa&#65533;&#65533;o como enfrentamento dos problemas sociais do pa&#65533;s. O Instituto 
-Semear acredita nas redes virtuosas formadas pela energia jovem que tem a educa&#65533;&#65533;o como 
+Educação como instrumento de desenvolvimento econômico e social: crença no poder 
+transformador da educação como enfrentamento dos problemas sociais do país. O Instituto 
+Semear acredita nas redes virtuosas formadas pela energia jovem que tem a educação como 
 principal instrumento 
 </p>
 <p>
-Efeito Multiplicador: o agente multiplicador &#65533; pe&#65533;a fundamental para o desenvolvimento de novos 
-sistemas virtuosos. Inspirar jovens a propagar os valores da iniciativa &#65533; um dos fundamentos do 
+Efeito Multiplicador: o agente multiplicador é peça fundamental para o desenvolvimento de novos 
+sistemas virtuosos. Inspirar jovens a propagar os valores da iniciativa é um dos fundamentos do 
 Instituto Semear. 
 </p>
 			<h4>Metodologias</h4>
 Para cumprir os objetivos idealizados na cultura do Instituto Semear, a metodologia de trabalho 
-est&#65533; baseada em tr&#65533;s frentes de trabalho: Bolsa de Estudo, Programa de Mentoria e Rede de 
+está baseada em três frentes de trabalho: Bolsa de Estudo, Programa de Mentoria e Rede de 
 Contatos/ Treinamentos. 
 <p>
 - Bolsa de Estudo 
-A bolsa &#65533; um aux&#65533;lio financeiro que visa, sobretudo, a reduzir a evas&#65533;o escolar desses estudantes 
-por falta de recursos financeiros. Evitar que bons alunos tenham a vida acad&#65533;mica prejudicada pela 
-falta de recursos &#65533; fundamental para o seu desenvolvimento. O valor dessas bolsas 
-&#65533; determinado 
-de acordo com uma m&#65533;dia dos gastos de transporte, moradia, alimenta&#65533;&#65533;o e outras necessidades 
+A bolsa é um auxílio financeiro que visa, sobretudo, a reduzir a evasão escolar desses estudantes 
+por falta de recursos financeiros. Evitar que bons alunos tenham a vida acadêmica prejudicada pela 
+falta de recursos é fundamental para o seu desenvolvimento. O valor dessas bolsas 
+é determinado 
+de acordo com uma média dos gastos de transporte, moradia, alimentação e outras necessidades 
 dos bolsistas. 
 </p>
 <p>
 - Programa de Mentoria 
-O Programa de Mentoria surge como forma de possibilitar uma forma&#65533;&#65533;o mais plena aos bolsistas. 
-Pessoas com grande experi&#65533;ncia de vida s&#65533;o identificadas para assumirem o papel de mentor e 
-orientador, criando de um v&#65533;nculo de confian&#65533;a com o seu mentorando, o bolsista. O objetivo 
-&#65533; 
+O Programa de Mentoria surge como forma de possibilitar uma formação mais plena aos bolsistas. 
+Pessoas com grande experiência de vida são identificadas para assumirem o papel de mentor e 
+orientador, criando de um vínculo de confiança com o seu mentorando, o bolsista. O objetivo é
 contribuir para o crescimento educacional, cultural e pessoal desses jovens. 
 </p>
 <p>
 - Rede de Contatos/ Treinamentos 
-O objetivo da Rede de Bolsistas &#65533; criar uma grande fam&#65533;lia. O estabelecimento de conex&#65533;es s&#65533;lidas 
-com os bolsistas faz parte da miss&#65533;o do Instituto, que visa a facilitar o acesso ao mercado de 
+O objetivo da Rede de Bolsistas é criar uma grande família. O estabelecimento de conexões sólidas 
+com os bolsistas faz parte da missão do Instituto, que visa a facilitar o acesso ao mercado de 
 trabalho, a cursos de idiomas e a programas e eventos de empresas parceiras. Para isso, o Instituto 
 organiza encontros com os bolsistas, oferecendo workshops, palestras e treinamentos importantes 
 para o desenvolvimento pessoal, intelectual e cultural dos jovens. 
@@ -217,7 +216,7 @@ para o desenvolvimento pessoal, intelectual e cultural dos jovens.
 		<img src="../facilitadores/images/semear.png"  class="facil">
 		<h5>Organizador:</h5> 
 		<img src="../facilitadores/images/cge.jpg"  class="facil">		
-		<p><strong>Copyright � Semear. All Rights Reserved.</strong></p>
+		<p><strong>Copyright © Semear. All Rights Reserved.</strong></p>
 	</div>
 	<!-- End Footer --></div>
 <!-- End Container -->
